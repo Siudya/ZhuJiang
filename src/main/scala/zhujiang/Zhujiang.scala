5aeb067cdd08706734ae3781b37d97cf6b897920
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2024-08-28 10:28:30 +0800

    [XJ] Add prefixing option

--- src/main/scala/zhujiang/Zhujiang.scala
+++ src/main/scala/zhujiang/Zhujiang.scala
@@ -1,11 +1,19 @@
 package zhujiang
 
 import chisel3.Module
+import chisel3.experimental.{ChiselAnnotation, annotate}
 import org.chipsalliance.cde.config.Parameters
+import sifive.enterprise.firrtl.NestedPrefixModulesAnnotation
 import xijiang.Ring
 
 class Zhujiang(implicit p: Parameters) extends ZJModule {
   require(p(ZJParametersKey).tfsParams.isEmpty)
+  if(p(ZJParametersKey).modulePrefix != "") {
+    val mod = this.toNamed
+    annotate(new ChiselAnnotation {
+      def toFirrtl = NestedPrefixModulesAnnotation(mod, p(ZJParametersKey).modulePrefix, true)
+    })
+  }
   private val localRing = Module(new Ring(true))
   private val csnRing = Module(new Ring(false))
   val tmp_local_io = IO(localRing.io.get.cloneType)
