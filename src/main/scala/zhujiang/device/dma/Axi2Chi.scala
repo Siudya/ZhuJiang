62cbefe0614a9ebab803538f8c7d7f79ec94e4b2
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2024-10-11 18:20:17 +0800

    feat(dma): Add DMA bridge wrapper connection examples

--- src/main/scala/zhujiang/device/dma/Axi2Chi.scala
+++ src/main/scala/zhujiang/device/dma/Axi2Chi.scala
@@ -18,18 +18,29 @@ class Axi2Chi(node: Node)(implicit p: Parameters) extends ZJModule {
   require(node.nodeType == NodeType.RI)
   private val dmaParams = zjParams.dmaParams
   private val axiParams = AxiParams(dataBits = dw, addrBits = raw, idBits = dmaParams.idBits)
+  val axi = IO(Flipped(new AxiBundle(axiParams)))
   val icn = IO(new DeviceIcnBundle(node))
-  val axi = IO(new AxiBundle(axiParams))
 
-  private val rxRespBits = WireInit(icn.rx.resp.get.bits.asTypeOf(new RespFlit))
-  private val rxDataBits = WireInit(icn.rx.data.get.bits.asTypeOf(new DataFlit))
   dontTouch(icn)
   dontTouch(axi)
+  icn.tx := DontCare
+  axi := DontCare
+
+  //How to use CHI rx flit
+  private val rxRespBits = WireInit(icn.rx.resp.get.bits.asTypeOf(new RespFlit))
+  private val rxDataBits = WireInit(icn.rx.data.get.bits.asTypeOf(new DataFlit))
   dontTouch(rxRespBits)
   dontTouch(rxDataBits)
 
+  //How to drive CHI tx flit
+  private val txReqFlit = Wire(new ReqFlit)
+  txReqFlit := DontCare
+  txReqFlit.Opcode := ReqOpcode.ReadOnce
+  txReqFlit.Addr := 0x12345678L.U
+  icn.tx.req.get.bits := txReqFlit.asTypeOf(icn.tx.req.get.bits)
+  icn.tx.req.get.valid := true.B
+
   icn.rx.resp.get.ready := false.B
   icn.rx.data.get.ready := false.B
-  icn.tx := DontCare
-  axi := DontCare
+
 }
